2497a8bfda891fc72ff52a96e96e31a64e8e75d5
WGS-94 <wilson-senx@hotmail.com>
2023-11-15 02:11:10 -0300

    :tada: Refatoring this app

--- lib/widget/text_fild.dart
+++ lib/widget/text_fild.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:flutter_todo_app/constants/colors.dart';
 import 'package:flutter_todo_app/constants/text_style.dart';
 
@@ -11,7 +12,7 @@ Widget textFild({
 }) {
   return Container(
     height: 54.0,
-    padding: const EdgeInsets.symmetric(horizontal: 30),
+    padding: const EdgeInsets.only(left: 20, right: 40),
     margin: const EdgeInsets.symmetric(
       horizontal: 20.0,
       vertical: 10.0,
@@ -23,9 +24,14 @@ Widget textFild({
     child: Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        const Icon(
-          Icons.vpn_key,
-          color: Color(0xffF5591F),
+        //  const Icon(
+        //   Icons.vpn_key,
+        //   color: Color(0xffF5591F),
+        // ),
+        SvgPicture.asset(
+          'assets/icon/$image',
+          height: 20.0,
+          color: green800,
         ),
         SizedBox(
           width: 270.0,
@@ -41,11 +47,6 @@ Widget textFild({
             style: headline2,
           ),
         ),
-        // SvgPicture.asset(
-        //   'assets/icon/$image',
-        //   height: 20.0,
-        //   color: grayText,
-        // )
       ],
     ),
   );
